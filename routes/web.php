df5346287b02111fe7eb5b81874080ff64eb6bd0
Azharusni <azharusnistp@gmail.com>
2021-09-24 12:05:40 +0700

    get third party API

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MoviesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::get('/',[MoviesController::class, 'index'])->name('movies.index');
+Route::get('/movie/{movie}',[MoviesController::class, 'show'])->name('movies.show');
 
-Route::view('/','index');
-Route::view('/movie','show');
+
